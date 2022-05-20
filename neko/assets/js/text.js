2200db89c640a2b9c974dc847babe6e4ca10d76e
y_neko <96345521+nekoo1@users.noreply.github.com>
2022-05-20 09:17:31 +0300

    update files web

---
+++ neko/assets/js/text.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".typed").typed({
+        strings: ["html", "css", "python", "coder",],
+        typeSpeed: 30,
+        backSpeed: 0,
+        cursorChar: ['❚'],
+        smartBackspace: true,
+        fadeOut: true,
+        loop: true,
+    });
+});
